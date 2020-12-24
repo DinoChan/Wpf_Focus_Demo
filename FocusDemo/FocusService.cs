837ab2eae21abdf56acab2fd741d226503b0dcbc
Jinhua Chen <Jinhua.Chen@omec-instruments.com>
2020-12-24 10:45:54 +0800

    add a demo

--- FocusDemo/FocusService.cs
+++ FocusDemo/FocusService.cs
@@ -40,13 +40,55 @@ namespace FocusDemo
                 return;
 
 
-            if (newValue == null)
+            if (string.IsNullOrWhiteSpace(newValue))
                 return;
 
-           
+
             var target = obj as FrameworkElement;
             if (target.Name == newValue)
                 target.Focus();
         }
+
+
+
+
+        /// <summary>
+        /// 从指定元素获取 FocusElement 依赖项属性的值。
+        /// </summary>
+        /// <param name="obj">从中读取属性值的元素。</param>
+        /// <returns>从属性存储获取的属性值。</returns>
+        public static string GetFocusElement(DependencyObject obj) => (string)obj.GetValue(FocusElementProperty);
+
+        /// <summary>
+        /// 将 FocusElement 依赖项属性的值设置为指定元素。
+        /// </summary>
+        /// <param name="obj">对其设置属性值的元素。</param>
+        /// <param name="value">要设置的值。</param>
+        public static void SetFocusElement(DependencyObject obj, string value) => obj.SetValue(FocusElementProperty, value);
+
+        /// <summary>
+        /// 标识 FocusElement 依赖项属性。
+        /// </summary>
+        public static readonly DependencyProperty FocusElementProperty =
+            DependencyProperty.RegisterAttached("FocusElement", typeof(string), typeof(FocusService), new PropertyMetadata(default(string), OnFocusElementChanged));
+
+
+        private static void OnFocusElementChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            var oldValue = (string)args.OldValue;
+            var newValue = (string)args.NewValue;
+            if (oldValue == newValue)
+                return;
+
+            if (string.IsNullOrWhiteSpace(newValue))
+                return;
+
+            var target = obj as FrameworkElement;
+            var focueElement = target.GetVisualDescendants().OfType<FrameworkElement>().FirstOrDefault(d => d.Name == newValue);
+            if (focueElement != null)
+                focueElement.Focus();
+        }
+
+
     }
 }
